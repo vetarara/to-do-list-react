264c417e0a2781870b135da264bf0fccda140c5f
Veta <vetarara@yandex.ru>
2026-06-05 01:26:44 +0300

    правит пути на страницу задачи

--- src/components/ListItem/ListItem.styled.ts
+++ src/components/ListItem/ListItem.styled.ts
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom"
 import styled from 'styled-components'
 
-export const ListItemLink = styled.a<{ $isDone: boolean }>`
+export const ListItemLink = styled(Link)<{ $isDone: boolean }>`
     text-decoration: none;
     padding: 10px;
     color: ${({ $isDone }) => ($isDone ? 'green' : 'red')};
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,13 +1,12 @@
 import { ToDo } from "../../models/todo-item"
+import { Link } from "react-router-dom"
 import { ListItemLink } from "./ListItem.styled"
 
 export const ListItem = ({todo}: { todo: ToDo }) => {
     return (
         <ListItemLink 
+            to={`/list/${todo.id}`}
             $isDone={todo.isDone}
-            target="_blank"
-            rel="noreferrer"
-            href={`/list/${todo.id}`}
         >{todo.text}</ListItemLink>
     )
 }
